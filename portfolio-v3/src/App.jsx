041851e80ddac13f1758a98cd265aa634d3ec39a
Noah Vogtli <noahvogtli88@gmail.com>
2026-03-11 20:58:11 -0400

    resume add

--- portfolio-v3/src/App.jsx
+++ portfolio-v3/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import Navbar from './components/navbar'
 import Hero from './components/hero'
 import About from './pages/About'
@@ -17,6 +17,7 @@ function Layout() {
         <Route path="/resume" element={<Resume />} />
         <Route path="/" element={<div className="w-[90dvw] md:w-[50dvw] mx-auto"><Hero /></div>} />
         <Route path="/about" element={<div className="w-[90dvw] md:w-[50dvw] mx-auto"><About /></div>} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       {!isResume && <Footer />}
     </>
