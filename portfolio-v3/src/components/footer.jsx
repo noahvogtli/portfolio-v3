818c37b52ecba35aaba12122e4adcd97fe69dd27
Noah Vogtli <noahvogtli88@gmail.com>
2026-03-11 23:04:07 -0400

    added project cards

--- portfolio-v3/src/components/footer.jsx
+++ portfolio-v3/src/components/footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Footer() {
     return (
         <footer className="w-full bg-[#171717] text-white pt-10 pb-10 mt-16 text-center text-sm border-t border-neutral-700">
-            <div className="text-xs md:text-sm flex flex-row w-[90%] md:w-[50%] mx-auto items-center justify-between">
+            <div className="text-xs md:text-sm flex flex-row w-[80%] md:w-[50%] mx-auto items-center justify-between">
                 <p>&copy; {new Date().getFullYear()} Noah Vogtli. All rights reserved.</p>
                 <p>
                     <Link to="/policy" className="text-neutral-400 hover:text-white transition-colors duration-200">Privacy Policy</Link>
