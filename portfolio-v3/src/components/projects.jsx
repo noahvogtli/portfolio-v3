74c6d83f7e5bb415b829cd21c16f9b7276a9971a
Noah Vogtli <noahvogtli88@gmail.com>
2026-03-12 11:57:46 -0400

    hover card

--- portfolio-v3/src/components/projects.jsx
+++ portfolio-v3/src/components/projects.jsx
@@ -10,7 +10,7 @@ function Projects(){
         <>
         <h2 className="text-white text-3xl uppercase tracking-widest mb-3 mt-20 mb-10">Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div className="bg-[#1e1e1e] border border-neutral-700 rounded-xl p-4 flex flex-col gap-4 text-neutral-300 text-sm leading-relaxed flex-2 h-[100%]">
+            <div className="bg-[#1e1e1e] border border-neutral-700 rounded-xl p-4 flex flex-col gap-4 text-neutral-300 text-sm leading-relaxed flex-2 h-[100%] hover:shadow-2xl hover:translate-y-[-2px] transition-all duration-300">
                 <img src={versevision} alt="VerseVision screenshot" className="w-full h-[50%] object-cover rounded-md"/>
                 <h1 className="text-lg">VerseVision</h1>
                 <p className="text-xs text-zinc-500">React | Anthropic | Render | Node.js | Express | Supabase</p>
@@ -20,7 +20,7 @@ function Projects(){
                     <a href="https://www.versevision.net" target="_blank" className="flex-1 bg-neutral-800 rounded-xl pl-4 pr-4 pt-2 pb-2 border border-neutral-700 inline-flex justify-center items-center gap-2 transition-colors duration-200 hover:bg-neutral-700 hover:border-neutral-600"><FaLink></FaLink>Visit</a>
                 </div>
             </div>
-            <div className="bg-[#1e1e1e] border border-neutral-700 rounded-xl p-4 flex flex-col gap-4 text-neutral-300 text-sm leading-relaxed flex-2 h-[100%]">
+            <div className="bg-[#1e1e1e] border border-neutral-700 rounded-xl p-4 flex flex-col gap-4 text-neutral-300 text-sm leading-relaxed flex-2 h-[100%] hover:shadow-2xl hover:translate-y-[-2px] transition-all duration-300">
                 <img src={digitalmedia} alt="21 Digital Media screenshot" className="w-full h-[50%] object-cover rounded-md"/>
                 <h1 className="text-lg">21 Digital Media</h1>
                 <p className="text-xs text-zinc-500">React | Netlify</p>
@@ -29,7 +29,7 @@ function Projects(){
                     <a href="https://www.21digital.media" target="_blank" className="flex-1 bg-neutral-800 rounded-xl pl-4 pr-4 pt-2 pb-2 border border-neutral-700 inline-flex justify-center items-center gap-2 transition-colors duration-200 hover:bg-neutral-700 hover:border-neutral-600"><FaLink></FaLink>Visit</a>
                 </div>
             </div>
-            <div className="bg-[#1e1e1e] border border-neutral-700 rounded-xl p-4 flex flex-col gap-4 text-neutral-300 text-sm leading-relaxed flex-2 h-[100%]">
+            <div className="bg-[#1e1e1e] border border-neutral-700 rounded-xl p-4 flex flex-col gap-4 text-neutral-300 text-sm leading-relaxed flex-2 h-[100%] hover:shadow-2xl hover:translate-y-[-2px] transition-all duration-300">
                 <img src={lavishlandscapes} alt="Lavish Landscapes screenshot" className="w-full h-[50%] object-cover rounded-md"/>
                 <h1 className="text-lg">Lavish Landscapes</h1>
                 <p className="text-xs text-zinc-500">React | Netlify</p>
@@ -38,7 +38,7 @@ function Projects(){
                     <a href="https://www.lavishlandscapesny.com" target="_blank" className="flex-1 bg-neutral-800 rounded-xl pl-4 pr-4 pt-2 pb-2 border border-neutral-700 inline-flex justify-center items-center gap-2 transition-colors duration-200 hover:bg-neutral-700 hover:border-neutral-600"><FaLink></FaLink>Visit</a>
                 </div>
             </div>
-            <div className="bg-[#1e1e1e] border border-neutral-700 rounded-xl p-4 flex flex-col gap-4 text-neutral-300 text-sm leading-relaxed flex-2 h-[100%]">
+            <div className="bg-[#1e1e1e] border border-neutral-700 rounded-xl p-4 flex flex-col gap-4 text-neutral-300 text-sm leading-relaxed flex-2 h-[100%] hover:shadow-2xl hover:translate-y-[-2px] transition-all duration-300">
                 <img src={javascriptwrestlers} alt="Javascript Wrestlers Screenshot" className="w-full h-[50%] object-cover rounded-md"/>
                 <h1 className="text-lg">Javascript Wrestlers</h1>
                 <p className="text-xs text-zinc-500">HTML | CSS | JavaScript</p>
